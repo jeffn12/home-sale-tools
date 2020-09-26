9924797c4666bf38fcb6c04c4dab765e8562fe1a
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-09-26 10:42:14 -0400

    feat: add input for commission rate

--- src/__tests__/DetailInput.test.js
+++ src/__tests__/DetailInput.test.js
@@ -3,9 +3,35 @@ import { render } from '@testing-library/react';
 import DetailInput from '../components/DetailInput';
 
 describe('DetailInput view', () => {
+  test('has 5 data inputs', () => {});
+
   test('has an input for amount owed', () => {
     const { getByLabelText } = render(<DetailInput />);
     const inputElement = getByLabelText(/amountOwed/i);
     expect(inputElement).toBeInTheDocument();
   });
+
+  test('has an input for commission rate', () => {
+    const { getByLabelText } = render(<DetailInput />);
+    const inputElement = getByLabelText(/commissionRate/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  /*   test('has an input for transfer tax rate', () => {
+    const { getByLabelText } = render(<DetailInput />);
+    const inputElement = getByLabelText(/transferTaxRate/i);
+    expect(inputElement).toBeInTheDocument();
+  }); */
+
+  /*   test('has an input for attorney fee', () => {
+    const { getByLabelText } = render(<DetailInput />);
+    const inputElement = getByLabelText(/attorneyFee/i);
+    expect(inputElement).toBeInTheDocument();
+  }); */
+
+  /*   test('has an input for other fees', () => {
+    const { getByLabelText } = render(<DetailInput />);
+    const inputElement = getByLabelText(/otherFees/i);
+    expect(inputElement).toBeInTheDocument();
+  }); */
 });
--- src/components/DetailInput.js
+++ src/components/DetailInput.js
@@ -4,6 +4,7 @@ import { Box, Slider, Typography } from '@material-ui/core';
 
 const DetailInput = () => {
   const [amountOwed, setAmountOwed] = useState(0);
+  const [commissionRate, setCommissionRate] = useState(0);
   const handleAmountOwedChange = (e, value) => setAmountOwed(value);
   return (
     <Box m={2}>
@@ -12,6 +13,12 @@ const DetailInput = () => {
         value={amountOwed}
         onChange={handleAmountOwedChange}
         aria-label="amountOwed"
+        max={1000000}
+      />
+      <Slider
+        value={commissionRate}
+        onChange={(e, value) => setCommissionRate(value)}
+        aria-label="commissionRate"
       />
     </Box>
   );
