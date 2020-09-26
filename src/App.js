2e9d88f1fb6a3b801c74e58e5c6595ed6a1b1fc3
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-09-26 10:13:49 -0400

    feat: add basic detailInput view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+// Views
+import { DetailInput } from './views';
 function App() {
   return (
     <div className="App">
       <h1>Let's Sell Some Houses!</h1>
+      <DetailInput />
     </div>
   );
 }
---
+++ src/views/DetailInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailInput = () => {
+  return (
+    <div>
+      <h4>Give Me The Deets:</h4>
+    </div>
+  );
+};
+
+export default DetailInput;
